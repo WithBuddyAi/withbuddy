5e46c793449b54a3f0e4e70c3709cf86288d5a11
Hong SH <132043678+aerofleet@users.noreply.github.com>
2026-04-01 15:49:38 +0900

    feat: 백엔드 서버용 초기 파일 및 기타 관련 문서 업데이트 (#64)
    * feat: initialize backend project with profile-based spring config
    
    * fix: build.gradle 버전 변경 및 테스트 완료
    
    * fix: build.gradle 버전 수정 - 테스트완료
    
    * fix: build.gradle 및 초기 파일 업데이트

---
+++ backend/src/main/java/com/withbuddy/WithbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.withbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithbuddyApplication.class, args);
+	}
+
+}
