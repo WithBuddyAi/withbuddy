b3c59afd5d8461d178fa4cd12989cdfb21019277
sumin16 <dcsmjswv2@gmail.com>
2026-04-07 14:48:38 +0900

    feat(frontend): 로그인 API 연동 및 UI 완성도 개선 [SCRUM-67] [SCRUM-107]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,29 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { useState } from 'react';
 import Login from './pages/Login'
+import MyBuddy from './pages/MyBuddy';
 import './App.css'
 
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    !!localStorage.getItem('accessToken')
+  )
   return (
     <div>
       <Routes>
         {/* 로그인 여부에 따른 페이지 이동 / 로그인 -> My Buddy 페이지, 미로그인 -> Login 페이지 */}
         <Route path="/" element={
-          localStorage.getItem('accessToken') ? 
+          isLoggedIn ? 
           <Navigate to='/mybuddy'/> : <Navigate to='/login'/>
         } />
-        <Route path="/login" element={ <Login/> } />
-        <Route path="/mybuddy" element={<div>마이버디</div>} />
+
+        <Route path="/login" element={ <Login setIsLoggedIn={setIsLoggedIn}/> } />
+
+        <Route path="/mybuddy" element={
+          isLoggedIn ?
+          <MyBuddy/> : <Navigate to='/login'/>
+        } />
       </Routes>
     </div>
   );
