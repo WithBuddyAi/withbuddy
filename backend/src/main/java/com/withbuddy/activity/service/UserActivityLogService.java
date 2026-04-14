802e0c48fc12adbb43b17bac02bcda0c5701351e
kim jiwon <wldnjsdl7452@gmail.com>
2026-04-14 13:07:40 +0900

    activity log 기능 구현

---
+++ backend/src/main/java/com/withbuddy/activity/service/UserActivityLogService.java
@@ -0,0 +1,78 @@
+package com.withbuddy.activity.service;
+
+import com.withbuddy.activity.dto.SessionStartLogResponse;
+import com.withbuddy.activity.entity.EventTarget;
+import com.withbuddy.activity.entity.EventType;
+import com.withbuddy.activity.entity.UserActivityLog;
+import com.withbuddy.activity.repository.UserActivityLogRepository;
+import com.withbuddy.global.jwt.JwtService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserActivityLogService {
+
+    private final UserActivityLogRepository userActivityLogRepository;
+    private final JwtService jwtService;
+
+    @Transactional
+    public void saveLoginSessionStart(Long userId) {
+        UserActivityLog log = new UserActivityLog(
+                userId,
+                EventType.SESSION_START,
+                EventTarget.LOGIN,
+                LocalDateTime.now()
+        );
+
+        userActivityLogRepository.save(log);
+    }
+
+    @Transactional
+    public SessionStartLogResponse saveChatSessionStart(String bearerToken) {
+        String token = bearerToken.replace("Bearer ", "");
+        Long userId = jwtService.getUserId(token);
+
+        LocalDateTime thirtyMinutesAgo = LocalDateTime.now().minusMinutes(30);
+
+        Optional<UserActivityLog> recentLog =
+                userActivityLogRepository
+                        .findTopByUserIdAndEventTypeAndEventTargetAndCreatedAtGreaterThanEqualOrderByCreatedAtDesc(
+                                userId,
+                                EventType.SESSION_START,
+                                EventTarget.CHAT,
+                                thirtyMinutesAgo
+                        );
+
+        if (recentLog.isPresent()) {
+            return new SessionStartLogResponse(
+                    false,
+                    EventType.SESSION_START.name(),
+                    EventTarget.CHAT.name(),
+                    "30분 이내 동일 사용자 채팅 진입 기록이 이미 존재합니다.",
+                    null
+            );
+        }
+
+        UserActivityLog savedLog = userActivityLogRepository.save(
+                new UserActivityLog(
+                        userId,
+                        EventType.SESSION_START,
+                        EventTarget.CHAT,
+                        LocalDateTime.now()
+                )
+        );
+
+        return new SessionStartLogResponse(
+                true,
+                savedLog.getEventType().name(),
+                savedLog.getEventTarget().name(),
+                null,
+                savedLog.getCreatedAt().toString()
+        );
+    }
+}
