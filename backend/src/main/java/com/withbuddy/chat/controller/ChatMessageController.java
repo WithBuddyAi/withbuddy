357bddf50aa6dde870ab2dbfa2a3465373703f1e
kim jiwon <wldnjsdl7452@gmail.com>
2026-05-01 09:59:00 +0900

    fix: interface

--- backend/src/main/java/com/withbuddy/chat/controller/ChatMessageController.java
+++ backend/src/main/java/com/withbuddy/chat/controller/ChatMessageController.java
@@ -34,7 +34,7 @@ import java.util.Map;
 @RequestMapping("/api/v1/chat")
 @RequiredArgsConstructor
 @Tag(name = "Ai", description = "버디 채팅 API")
-public class ChatMessageController implements ChatMessageControllerDocs {
+public class ChatMessageController {
 
     private final ChatMessageService chatMessageService;
     private final ChatMessageQueryService chatMessageQueryService;
