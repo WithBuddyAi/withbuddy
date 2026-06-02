c9a0c3be326581c9ac5751e380cc0b9fcad48a09
sumin16 <dcsmjswv2@gmail.com>
2026-06-02 16:14:52 +0900

    feat(frontend): sync frontend changes from develop

--- frontend/src/components/admin/AdminMainView/UserMobileCard.jsx
+++ frontend/src/components/admin/AdminMainView/UserMobileCard.jsx
@@ -17,24 +17,32 @@ function UserMobileCard({ user }) {
         <span className="text-[16px] font-medium text-[#000000]">
           {user.name}
         </span>
-        {/* API 업데이트 되면 맞는 값으로 수정 */}
-        <span className="text-[12px] text-[#868E96]">-</span>
+        <span className="px-[12px]">
+          {user.role === "ACTIVE" ? (
+            <div className="rounded-[16px] py-[4px] px-[12px] bg-[#EBFBEE] text-[#37B24D] text-[12px]">
+              <span className="text-[8px] mr-[4px]">●</span>활성화
+            </div>
+          ) : (
+            <div className="rounded-[16px] py-[4px] px-[12px] bg-[#F1F3F5] text-[#868E96] text-[12px]">
+              <span className="text-[8px] mr-[4px]">●</span>비활성화
+            </div>
+          )}
+        </span>
       </div>
 
       {/* 부서(팀) */}
-      <p className="text-[14px] text-[#495057] mb-[12px]">
-        {user.department}({user.teamName})
-      </p>
+      <p className="text-[14px] text-[#495057] mb-[12px]">{user["부서(팀)"]}</p>
 
       {/* 상세정보 */}
       <div className="flex flex-col gap-[6px] text-[14px]">
         <InfoRow label="사번" value={user.employeeNumber} />
         <InfoRow label="입사일" value={user.hireDate} />
         <InfoRow label="입사일차" value={`D+${user.hireDay}`} />
-        {/* API 업데이트 되면 맞는 값으로 수정 */}
-        <InfoRow label="마지막 접속" value="-" />
-        {/* API 업데이트 되면 맞는 값으로 수정 */}
-        <InfoRow label="질문 횟수" value="-" />
+        <InfoRow label="마지막 접속" value={user.lastLoginDate ?? "-"} />
+        <InfoRow
+          label="질문 횟수"
+          value={user.questionCount != null ? `${user.questionCount}회` : "-"}
+        />
       </div>
     </div>
   );
